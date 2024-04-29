502f55fc404b2704c75d32ba282c0d41cbfd9933
Yousung Jung <jys01012@gmail.com>
2024-04-29 23:31:00 +0900

    ✨ Winston logger (#6)
    * chore: winston 패키지 추가
    
    * nest winston 추가
    
    * chore: nest config 패키지 추가
    
    * chore: winston daily rotate file 패키지 추가
    
    * chore: nest cli에 apps, libs 등록
    
    * chore: api 실행 명령어 설정
    
    * feat: winston logger 추가
    
    	nest 내장 Logger 대신 winston logger 사용
    	logs/.. 에 error 폴더와 info 폴더로 로그가 저장되도록 함
    
    * rename: logger module 위치 변경
    
    * fix: ts path 와 jest path를 사용하여 logger를 불러오지 못하는 버그 수정
    
    	path 설정 후 test 완료.

---
+++ libs/utils/src/logger/logger.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { LoggerService } from './logger.service';
+@Module({
+  imports: [WinstonModule.forRootAsync({ useClass: LoggerService })],
+})
+export class LoggerModule {}
