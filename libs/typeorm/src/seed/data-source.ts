dbcd1f0143508dac992f19305652b3643d5a48e3
can019 <jys01012@gmail.com>
2024-05-01 03:48:27 +0900

    refactor: 여러개였던 ormconfig를 하나로 리팩토링 	TypeORM seeding / TypORM - nest 를 하나로 리팩터 	코드 가독성, 중복 해결

--- libs/typeorm/src/seed/data-source.ts
+++ libs/typeorm/src/seed/data-source.ts
@@ -1,15 +1,20 @@
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
-import { defaultOrmConfig } from '@libs/typeorm/ormconfig';
+import { setOrmConfig } from '../ormconfig';
 import UserSeeder from './user.seeder';
 import userFactory from '../factory/user.factory';
 
+config({ path: '.env.local' });
+const configService = new ConfigService({ app: { env: 'test' } });
+
 const options: DataSourceOptions & SeederOptions = {
-  ...defaultOrmConfig(),
+  ...(setOrmConfig(configService) as DataSourceOptions),
   logging: process.env.TEST === 'enabled' ? false : true,
 
   seeds: [UserSeeder],
   factories: [userFactory],
 };
 
-export const dataSourceForSeeding = new DataSource(options);
+export default new DataSource(options);
