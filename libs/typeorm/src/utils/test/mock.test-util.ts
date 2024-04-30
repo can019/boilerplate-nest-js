54c840fc2a43c3a7a360afafb4f9e1a0262c008d
Yousung Jung <jys01012@gmail.com>
2024-05-01 01:42:47 +0900

    🐛 Unit / e2e test 정상화 (#13)
    * rename: 파일 이동
    
    * test: unit, e2e 분리를 위해 디렉토리 수정
    
    * chore: test 타켓 파일 명을 *.unit-spec.*.ts로 변경
    
    	기존 testRegex는 .e2e-spec.ts
    
    * rename: app controller unit test 위치 변경
    
    * test: unit test
    
    	unit test를 위한 user service, controller, repository 작성.
    		- 실 로직이 아닌 임시 로직
    
    	DataSoure mokcing
    		- DataSource class를 mokcing한 DataSourceMock class 작성.
    
    * ci: api unit test workflow 추가
    
    * fix: pakcage.json에 unit test coverage 명령어 추가
    
    * chore: mock extended package를 통해 typeorm 모킹
    
    	모킹은 정상적으로 되나 typeorm 관련 함수들이 무조건 Undefined를 반환
        - spyOn이나 return value를 확인 할 수 없음.
    
    * remove: 필요 없는 Import 제거
    
    * fix: report.json W 권한 부여
    
    * fix: api-local에만 write 부여
    
    * ci: permission 모두 open
    
    * style: fix lint
    
    * fix: e2e local 테스트 스크립트 수정
    	jest-e2e.json 위치가 잘못 잡혀있는 버그 픽스
    
    * chore: 오타 수정

--- libs/typeorm/src/utils/test/mock.test-util.ts
+++ libs/typeorm/src/utils/test/mock.test-util.ts
@@ -1,4 +1,33 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { DataSource, EntityManager, QueryRunner, createQueryBuilder } from 'typeorm';
+
 /* eslint-disable @typescript-eslint/no-empty-function */
 export class DataSoureMock {
   async transaction(): Promise<void> {}
 }
+export class EntityManagerMock {
+  save() {}
+  delete() {}
+}
+
+export type MockType<T> = {
+  [P in keyof T]?: jest.Mock<{}>;
+};
+
+export const dataSourceMockFactory: () => MockType<DataSource> = jest.fn(() => ({
+  createQueryRunner: jest.fn().mockImplementation(() => ({
+    connect: jest.fn(),
+    startTransaction: jest.fn(),
+    release: jest.fn(),
+    rollbackTransaction: jest.fn(),
+    manager: {
+      save: jest.fn(),
+      delete: jest.fn(),
+    },
+  })),
+  manager: jest.fn()(() => ({
+    save: jest.fn(),
+    delete: jest.fn(),
+  })),
+  transaction: jest.fn(),
+}));
