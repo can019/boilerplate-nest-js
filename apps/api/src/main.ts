295c22c5788914bb5b55fa112430903cfcb1ca5b
can019 <jys01012@gmail.com>
2024-05-01 11:14:03 +0900

    feat: reate limit 설정

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,20 +4,29 @@ import { WINSTON_MODULE_NEST_PROVIDER, WINSTON_MODULE_PROVIDER } from 'nest-wins
 import { LoggingInterceptor } from '@libs/interceptor/http-logging.Interceptor';
 import { GlobalExceptionFilter } from '@libs/filter/global-exception.filter';
 import rateLimit from 'express-rate-limit';
-import { rateLimitOption } from './config/secure/rate-limit.config';
-import { corsOption } from './config/secure/cors.config';
+import { rateLimitOption } from '@api/src/config/secure/rate-limit.config';
+import { corsOption } from '@api/src/config/secure/cors.config';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true, // false로 바꿀 경우 bootstrap시 로그는 winston이 아닌 nest 자체 로그 사용.
   });
 
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-  app.useGlobalInterceptors(new LoggingInterceptor(app.get(WINSTON_MODULE_PROVIDER)));
-  app.useGlobalFilters(new GlobalExceptionFilter(app.get(WINSTON_MODULE_NEST_PROVIDER))); // 전역 필터 적
-
+  /* Secure*/
   app.enableCors(corsOption);
   app.use(rateLimit(rateLimitOption)); //호출 제한
+  /** */
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER)); // winston logger 설정
+
+  /* Custom interceptor / pipe / guard / filter */
+  app.useGlobalInterceptors(new LoggingInterceptor(app.get(WINSTON_MODULE_PROVIDER))); // http 로깅 인터셉터
+  app.useGlobalFilters(new GlobalExceptionFilter(app.get(WINSTON_MODULE_NEST_PROVIDER))); // 전역 필터 적
+  /** */
 
   app.setGlobalPrefix('api');
   await app.listen(3000);
