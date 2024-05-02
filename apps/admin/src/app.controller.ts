7ac60d22eb4fd0a63e3c5b4d6a2a7f40c85171de
Yousung Jung <jys01012@gmail.com>
2024-05-03 02:25:32 +0900

    🧑‍💻 각 프로젝트에서 이용 가능한 ts path alias 제한 (#25)
    * feat: admin
      - initial 상태, 기능은 없으며 api를 복제했음
    
    * refactor: 프로젝트 내에서 접근 가능한 ts path alias 제한
    
      1. Admin에서 `@api/**/*`, api에서 `@admin/**/*` path를 사용 가능을 못하게 제한
      2. 1과 같이 jest conifg에도 path alias 반영
      3. 2를 해결하기 위해 jest config의 확장자를 json에서 js로 변경
    
    * fix: admin build
    
    * ci: eslint만 체크하도록 변경
    
    * ci: workflow 이름 변경
    
    * ci: build test 분리
    
    * ci: Build test 트리거 조건 변경
    
    * fix: build test yml 오류로 workflow가 작동되지 않던 버그 수정
    
    	jobs 목록들의 제목에 띄어쓰기 포함된 것을 '-'로 변경

---
+++ apps/admin/src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { AppService } from '@admin/src/app.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    this.logger.info('called');
+    return this.appService.getHello();
+  }
+}
---
+++ apps/api/test/e2e/jest-e2e.js
@@ -0,0 +1,53 @@
+<<<<<<< HEAD
+{
+  "moduleFileExtensions": ["js", "json", "ts"],
+  "rootDir": "../../../../",
+  "testEnvironment": "node",
+  "testRegex": ".e2e-spec.ts$",
+  "transform": {
+    "^.+\\.(t|j)s$": "ts-jest"
+  },
+  "roots": ["<rootDir>/apps/", "<rootDir>/libs/"],
+  "moduleNameMapper": {
+    "^@libs(|/.*)$": "<rootDir>/libs/src/$1",
+    "^@app/test(|/.*)$": "<rootDir>/libs/test/src/$1",
+    "^@api/test(|/.*)$": "<rootDir>/apps/api/test/$1",
+    "^@api/src(|/.*)$": "<rootDir>/apps/api/src/$1"
+  }
+}
+=======
+module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    // ts-jest에서 tsconfig를 지정하지 않으면 rootDir에 있는 tsconfig가 적용됨.
+    // <rootDir>에 있는 tsconfig에는 path alias가 지정되어 있지 않음.
+    // moduleNameMapper option에 있는 alias는 ts-jest가 읽어들인 tsconfig에 있는 ts-path와 매칭시킴.
+    // <rootDir>을 '.' 로 설정하면 현재 디렉토리 하위 디렉토리만 접근 가능하게됨. 즉 libs는 접근이 불가능
+    // libs에 접근하지 못하면 정의된 test util들을 사용 불가능하게 됨.
+    'ts-jest': {
+      tsConfig: './apps/api/tsconfig.app.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  rootDir: '../../../../',
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coveragePathIgnorePatterns: ['node_modules'], // full path를 적어주어야 함. dir단위도 좋음.
+  coverageDirectory: './coverage',
+  roots: ['<rootDir>/apps/api', '<rootDir>/libs/'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transformIgnorePatterns: ['^.+\\.js$'],
+  testMatch: ['**/api/test/e2e/**/*.e2e-spec.ts'],
+  setupFiles: ['<rootDir>/libs/src/utils/test/testSetUp.ts'],
+  clearMocks: true,
+  verbose: true,
+  moduleNameMapper: {
+    '^@libs(|/.*)$': '<rootDir>/libs/src/$1',
+    '^@app/test(|/.*)$': '<rootDir>/libs/test/src/$1',
+    '^@api/test(|/.*)$': '<rootDir>/apps/api/test/$1',
+    '^@api/src(|/.*)$': '<rootDir>/apps/api/src/$1',
+  },
+};
+>>>>>>> c4cca00 (rename: jest.json -> jest.js)
